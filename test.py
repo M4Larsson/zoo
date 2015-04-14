ae42b75fd120c15c67dd17f84427442df1c07bac
ledhead <paraschakis@gmail.com>
2015-04-14 15:30:07 +0200

    changed index.html

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,12 +6,6 @@ def hello_world():
 	author='Dimitris'
 	name='from the Zoo!'
 	return render_template('index.html', author=author, name=name)
-	
-@app.route('/signup', methods = ['POST'])
-def signup():
-	email = request.form['email']
-	print("The email address is '" + email + "'")
-	return redirect('/')	
 
 if __name__ == '__main__':
     app.run()
